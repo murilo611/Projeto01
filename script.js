7f12d352b0ec0146e71525c4155bd8c396f57974
Murilo Boaro <m.boaro@outlook.com>
2025-02-04 08:30:14 +0000

    aa
    aaa

--- script.js
+++ script.js
@@ -8,16 +8,58 @@ document.addEventListener("DOMContentLoaded", function () {
     menuToggle.classList.toggle("active");
   });
 
-  // Código do Carrossel
-  let currentIndex = 0;
-  const images = document.querySelectorAll(".carousel img");
-  const totalImages = images.length;
+  // Função para inicializar o carrossel da seção "home"
+  function initCarouselHome() {
+    let currentIndexHome = 0;
+    const carouselHomeImages = document.querySelectorAll("#home .carousel img");
+    const totalImagesHome = carouselHomeImages.length;
 
-  function showNextImage() {
-    images[currentIndex].classList.remove("active");
-    currentIndex = (currentIndex + 1) % totalImages;
-    images[currentIndex].classList.add("active");
+    function showNextImageHome() {
+      carouselHomeImages[currentIndexHome].classList.remove("active");
+      currentIndexHome = (currentIndexHome + 1) % totalImagesHome;
+      carouselHomeImages[currentIndexHome].classList.add("active");
+    }
+
+    setInterval(showNextImageHome, 2000); // Mudar de imagem a cada 2 segundos
   }
 
-  setInterval(showNextImage, 2000); // Mudar de imagem a cada 2 segundos
+  // Função para inicializar o carrossel da seção "obras"
+  function initCarouselObras() {
+    let currentIndexObras = 0;
+    const prevBtn = document.querySelector("#carousel-obras .prev-btn");
+    const nextBtn = document.querySelector("#carousel-obras .next-btn");
+    const carouselImages = document.querySelector(
+      "#carousel-obras .carousel-images"
+    );
+    const images = document.querySelectorAll("#carousel-obras .static-image");
+    const totalImagesObras = images.length;
+
+    function updateCarouselObras() {
+      carouselImages.style.transform = `translateX(-${
+        currentIndexObras * 100
+      }%)`;
+    }
+
+    prevBtn.addEventListener("click", () => {
+      if (currentIndexObras > 0) {
+        currentIndexObras--;
+      } else {
+        currentIndexObras = totalImagesObras - 1; // Voltar à última imagem
+      }
+      updateCarouselObras();
+    });
+
+    nextBtn.addEventListener("click", () => {
+      if (currentIndexObras < totalImagesObras - 1) {
+        currentIndexObras++;
+      } else {
+        currentIndexObras = 0; // Voltar à primeira imagem
+      }
+      updateCarouselObras();
+    });
+  }
+
+  // Inicializar os carrosséis
+  initCarouselHome();
+  initCarouselObras();
 });
